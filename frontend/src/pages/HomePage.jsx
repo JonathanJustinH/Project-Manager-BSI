fcb2461f05927842b37e3d49e93c7adf13d397b9
Jonathan Justin Haryanto <j494878@gmail.com>
2026-06-25 15:56:09 +0700

    Update homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,21 +3,55 @@ import "./HomePage.css";
 function HomePage({ backendMessage }) {
   return (
     <>
-      <section className="feature-grid" aria-label="Homepage features">
-        <article>
-          <h2>Projects</h2>
-        </article>
+      <section className="feature-grid grid-5" aria-label="Homepage features">
+        <div>
+          <article>                     {/* angkanya masi placeholder */}
+            <h2>Projects</h2>
+            <h1>3</h1>
+            <p>Active Projects</p>
+          </article>
+        </div>
         <article>
           <h2>Tasks</h2>
+          <h1>65</h1>
+          <p>12 in Progress</p>
+        </article>
+        <article>
+          <h2>Tickets</h2>
+          <h1>11</h1>
+          <p>3 Urgent</p>
+        </article>
+        <article>
+          <h2>Documents</h2>
+          <h1>7</h1>
+          <p>Need Review</p>
+        </article>
+        <article>
+          <h2>Schedule</h2>
+          <h1>2</h1>
+          <p>To Attend</p>
+        </article>
+      </section>
+      <section className="feature-grid grid-3" aria-label="Homepage features">
+        <article>
+          <h2>Projects Progress</h2>
+        </article>
+        <article>
+          <h2>Tasks Overview</h2>
         </article>
         <article>
           <h2>Tickets</h2>
         </article>
+      </section>
+      <section className="feature-grid grid-3" aria-label="Homepage features">
         <article>
-          <h2>Documents</h2>
+          <h2>Document Status</h2>
         </article>
         <article>
-          <h2>Schedule</h2>
+          <h2>Activity Feed</h2>
+        </article>
+        <article>
+          <h2>Calendar</h2>
         </article>
       </section>
     </>
