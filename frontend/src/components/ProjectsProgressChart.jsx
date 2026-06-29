cf34aea5310c1b268f3e8396c34b0789d279ea5e
Jonathan Justin Haryanto <j494878@gmail.com>
2026-06-29 10:38:59 +0700

    Update homepage progress chart component

--- frontend/src/components/ProjectsProgressChart.jsx
+++ frontend/src/components/ProjectsProgressChart.jsx
@@ -5,12 +5,15 @@ import {
   XAxis,
   YAxis,
   Tooltip,
+  CartesianGrid,
 } from "recharts";
 
 const data = [
-  { name: "1", progress: 85 },
-  { name: "2", progress: 62 },
-  { name: "3", progress: 40 },
+  { name: "B-Shine Omnichannel", progress: 85 },
+  { name: "Project", progress: 62 },
+  { name: "Project 3", progress: 40 },
+  { name: "Project 4", progress: 90 },
+  { name: "Project 5", progress: 10 },
 ];
 
 function ProjectsProgressChart() {
@@ -21,14 +24,36 @@ function ProjectsProgressChart() {
         layout="vertical"
         margin={{ top: 10, right: 20, left: 20, bottom: 10 }}
       >
-        <XAxis type="number" domain={[0, 100]} />
+        <CartesianGrid
+          stroke="var(--border)"
+          horizontal={false}
+          vertical={true}
+          strokeDasharray="3 3"
+        />
+
+        <XAxis
+          type="number"
+          domain={[0, 100]}
+          tickFormatter={(value) => `${value}%`}
+          tick={{ fontSize: 16 }}
+        />
+
         <YAxis
           type="category"
           dataKey="name"
           width={80}
+          tick={{ fontSize: 16 }}
         />
+
         <Tooltip formatter={(value) => `${value}%`} />
-        <Bar className="progress-bar" dataKey="progress" radius={[0, 6, 6, 0]} fill="var(--border)"/>
+
+        <Bar
+          className="progress-bar"
+          dataKey="progress"
+          radius={0}
+          barSize={30}
+          fill="var(--border)"
+        />
       </BarChart>
     </ResponsiveContainer>
   );
